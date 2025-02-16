aaf3fa44ba22ddd3fda5a1e193d61d86e3316367
Hy Huynh <giahy33@gmail.com>
2025-02-16 00:15:56 -0700

    Redesign terminal

--- src/expressEntry/Terminal.ts
+++ src/expressEntry/Terminal.ts
@@ -1,105 +1,36 @@
 import { Database } from '../database/Database';
 import { Guid } from "../common/Guid";
 
-type FactorKey = 'coreHumanCapitalFactors' | 'spouseOrCommonLawPartnerFactors' | 'skillTransferabilityFactors' | 'additionalPointsFactors';
+type Relationship = "dependant" | "children" | "sibling";
 
-interface ExpressionScore {
-    expression: boolean;
+interface Group {
+    label: string;
+    terminals: Terminal[];
+    scoreExpression: string;
     score: number;
 }
 
-export interface Option {
-   id: Guid;
-   label: string;
+class Connector {
+    constructor(
+        public label: string,
+        public relationship: Relationship,
+        public nextGroup: Group[],
+        public prevTerminal: Terminal
+    ) {}
+
+    public addGroup(group: Group) {
+        this.nextGroup.push(group);
+    }
 }
 
 export class Terminal {
-    public children: Terminal[] = [];
-    // Map of selected value GUID to children GUIDs
-    public cacheChildren = new Map<Guid, Terminal[]>();
 
-    constructor(public id: Guid,
-                public valueGuidToChildrenGuids: Map<Guid, Guid[]>, public label: string,
-                public description: string, public options: Option[],
-                public value?: Option, public expressionScore?: ExpressionScore[],
-                public score: number = 0,
-                public parent: Terminal = this,
+    constructor(
+        public label: string,
+        public nextConnectors: Connector[] = [],
     ) {}
 
-    private async getNextTerminal(): Promise<Terminal[]> {
-        if (this.valueGuidToChildrenGuids.size == 0) {
-            return [];
-        }
-        if (!this.value) {
-            throw new Error('Value is not set');
-        }
-        if (!this.valueGuidToChildrenGuids.has(this.value.id)) {
-            throw new Error(`Value cannot be found: ${this.value.id, this.value.label}`);
-        }
-        const db = await Database.getInstance();
-        const terminalCollection = db.getDb().collection<Terminal>('terminals');
-        const nextTerminals = await terminalCollection.find({ id: { $in: this.valueGuidToChildrenGuids.get(this.value.id) } }).toArray();
-        if (!nextTerminals) {
-            throw new Error('No terminals found for the given children GUIDs');
-        }
-        return nextTerminals.map(terminalData => new Terminal(
-            terminalData.id,
-            new Map<Guid, Guid[]>(Object.entries(terminalData.valueGuidToChildrenGuids)),
-            terminalData.label,
-            terminalData.description,
-            terminalData.options,
-            terminalData.value,
-            terminalData.expressionScore,
-            terminalData.score,
-            this
-        ));
-    }
-
-    public async setValue(value: Option): Promise<Terminal[]> {
-        // If the value is the same as the current value, return
-        if (this.value == value){
-            return [];
-        }
-        this.value = value;
-        if(this.cacheChildren.has(value.id)){
-            this.children = this.cacheChildren.get(value.id)!;
-            return this.children;
-        }
-        this.children = []; // Clear old children
-        const nextTerminals = await this.getNextTerminal();
-        this.cacheChildren.set(value.id, nextTerminals);
-        // If there are no next terminals, then the current terminal is a leaf node
-        if (nextTerminals.length == 0) {
-            this.updateScore();
-        }
-        for (const terminal of nextTerminals) {
-            this.subscribeChild(terminal);
-        }
-        return nextTerminals;
-    }
-
-    public getValue(): Option {
-        if (!this.value) throw new Error('Value is not set');
-        return this.value;
-    }
-
-    public subscribeChild(child: Terminal): void {
-        this.children.push(child);
-    }
-
-    public async updateScore(): Promise<void> {
-        if (this.children.length > 0) {
-            if (this.expressionScore && this.expressionScore.length > 0) {
-                for (const expScore of this.expressionScore) {
-                    if (expScore.expression) {
-                        this.score = expScore.score;
-                    }
-                }
-            }
-        }
-
-        if (this.parent !== this) {
-            await this.parent.updateScore();
-        }
+    public addConnector(connector: Connector) {
+        this.nextConnectors.push(connector);
     }
 }
